e128ed1144064ba9ad4f743c57b334610bf6f86b
0Websites <vedansshderashri@gmail.com>
2025-03-28 23:49:42 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,60 @@
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: "auto",
+    spaceBetween: 20,
+    centeredSlides: true,
+    loop: false,
+    initialSlide: 1,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    effect: "coverflow",
+    coverflowEffect: {
+        rotate: 0,
+        stretch: 0,
+        depth: 150,
+        modifier: 2,
+        slideShadows: false,
+    },
+    breakpoints: {
+        1400: {
+            slidesPerView: "auto",
+        },
+        1024: {
+            slidesPerView: "auto",
+        },
+        768: {
+            slidesPerView: 1,
+        }
+    },
+    on: {
+        slideChangeTransitionEnd: function () {
+            document.querySelectorAll('.swiper-slide').forEach(slide => {
+                slide.style.transform = "scale(1)"; // Reset all slides
+            });
+            document.querySelector('.swiper-slide-active').style.transform = "scale(1.1)"; // Scale active slide
+        }
+    }
+});
+
+
+
+
+
+const hamBtn = document.getElementById("hambar");
+    const closeBtn = document.getElementById("close");
+    const menu = document.getElementById("hide");
+
+    hamBtn.addEventListener("click", () => {
+        menu.style.display = "flex"; // Show the div
+    });
+
+    closeBtn.addEventListener("click", () => {
+        menu.style.display = "none"; // Hide the div
+    });
+
+  
